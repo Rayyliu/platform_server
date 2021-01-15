27ffa2f5468e1dd4a17ac24729830de1cb90070a
LiuRui <liurui@xyd.cn>
2021-01-15 13:51:24 +0800

    新增日志配置文件logback.xml

---
+++ src/main/java/com/platform/config/NacosConfig.java
@@ -0,0 +1,54 @@
+package com.platform.config;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.cloud.alibaba.nacos.registry.NacosAutoServiceRegistration;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+
+import javax.management.MBeanServer;
+import javax.management.ObjectName;
+import javax.management.Query;
+import java.lang.management.ManagementFactory;
+import java.util.Set;
+
+@Component
+public class NacosConfig implements ApplicationRunner {
+
+
+    @Autowired(required = false)
+    private NacosAutoServiceRegistration registration;
+
+    @Value("${server.port}")
+    Integer port;
+
+
+    @Override
+    public void run(ApplicationArguments args) {
+        if (registration != null && port != null) {
+            Integer tomcatPort = port;
+            try {
+                tomcatPort = new Integer(getTomcatPort());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            registration.setPort(tomcatPort);
+            registration.start();
+        }
+    }
+
+    public String getTomcatPort() throws Exception {
+        MBeanServer beanServer = ManagementFactory.getPlatformMBeanServer();
+        Set<ObjectName> objectNames = beanServer.queryNames(new ObjectName("*:type=Connector,*"), Query.match(Query.attr("protocol"), Query.value("HTTP/1.1")));
+        String port = objectNames.iterator().next().getKeyProperty("port");
+        return port;
+    }
+
+    }
